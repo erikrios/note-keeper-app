cf0825b18ade34d350ce1e6fc5f3edf9678834f6
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-06-11 21:07:02 +0700

    Create NoteDetail widget

---
+++ lib/screens/note_detail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NoteDetail extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _NoteDetailState();
+  }
+}
+
+class _NoteDetailState extends State<NoteDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
