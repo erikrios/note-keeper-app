01182fcff3c974f5ce47784fad61ed63215e12a9
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-06-12 20:53:13 +0700

    Add WillPopScope widget

--- lib/screens/note_detail.dart
+++ lib/screens/note_detail.dart
@@ -35,130 +35,135 @@ class _NoteDetailState extends State<NoteDetail> {
   @override
   Widget build(BuildContext context) {
     TextStyle textStyle = Theme.of(context).textTheme.headline6;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          _appBarTitle,
-        ),
-        leading: IconButton(
-          icon: Icon(
-            Icons.arrow_back,
+    return WillPopScope(
+      onWillPop: () {
+        _navigateToList();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            _appBarTitle,
           ),
-          onPressed: () {
-            _navigateToList();
-          },
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+            ),
+            onPressed: () {
+              _navigateToList();
+            },
+          ),
         ),
-      ),
-      body: Padding(
-        padding: EdgeInsets.only(
-          left: _minimumPadding * 2,
-          top: _minimumPadding * 3,
-          right: _minimumPadding * 2,
-        ),
-        child: ListView(
-          children: [
-            ListTile(
-              title: DropdownButton(
-                items: _priorities.map((String priority) {
-                  return DropdownMenuItem<String>(
-                    child: Text(priority),
-                    value: priority,
-                  );
-                }).toList(),
-                onChanged: (String newValue) {
-                  setState(() {
-                    _priorityValue = newValue;
-                  });
-                  debugPrint('User selected $_priorityValue');
-                },
-                style: textStyle,
-                value: _priorityValue,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                top: _minimumPadding * 3,
-                bottom: _minimumPadding * 3,
-              ),
-              child: TextField(
-                controller: titleController,
-                style: textStyle,
-                decoration: InputDecoration(
-                  labelText: 'Title',
-                  labelStyle: textStyle,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(5.0),
-                  ),
+        body: Padding(
+          padding: EdgeInsets.only(
+            left: _minimumPadding * 2,
+            top: _minimumPadding * 3,
+            right: _minimumPadding * 2,
+          ),
+          child: ListView(
+            children: [
+              ListTile(
+                title: DropdownButton(
+                  items: _priorities.map((String priority) {
+                    return DropdownMenuItem<String>(
+                      child: Text(priority),
+                      value: priority,
+                    );
+                  }).toList(),
+                  onChanged: (String newValue) {
+                    setState(() {
+                      _priorityValue = newValue;
+                    });
+                    debugPrint('User selected $_priorityValue');
+                  },
+                  style: textStyle,
+                  value: _priorityValue,
                 ),
-                onChanged: (String value) {
-                  debugPrint('Something changed in Title Text Field');
-                },
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                top: _minimumPadding * 3,
-                bottom: _minimumPadding * 3,
-              ),
-              child: TextField(
-                controller: descriptionController,
-                style: textStyle,
-                decoration: InputDecoration(
-                  labelText: 'Description',
-                  labelStyle: textStyle,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(5.0),
-                  ),
+              Padding(
+                padding: EdgeInsets.only(
+                  top: _minimumPadding * 3,
+                  bottom: _minimumPadding * 3,
                 ),
-                onChanged: (String value) {
-                  debugPrint('Something changed in Description Text Field');
-                },
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                top: _minimumPadding * 3,
-                bottom: _minimumPadding * 3,
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: RaisedButton(
-                      color: Theme.of(context).primaryColorDark,
-                      textColor: Theme.of(context).primaryColorLight,
-                      child: Text(
-                        'Save',
-                        textScaleFactor: 1.5,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          debugPrint('Save state button clicked');
-                        });
-                      },
+                child: TextField(
+                  controller: titleController,
+                  style: textStyle,
+                  decoration: InputDecoration(
+                    labelText: 'Title',
+                    labelStyle: textStyle,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
                     ),
                   ),
-                  Container(
-                    width: 5.0,
-                  ),
-                  Expanded(
-                    child: RaisedButton(
-                      color: Theme.of(context).primaryColorDark,
-                      textColor: Theme.of(context).primaryColorLight,
-                      child: Text(
-                        'Delete',
-                        textScaleFactor: 1.5,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          debugPrint('Delete state button clicked');
-                        });
-                      },
+                  onChanged: (String value) {
+                    debugPrint('Something changed in Title Text Field');
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  top: _minimumPadding * 3,
+                  bottom: _minimumPadding * 3,
+                ),
+                child: TextField(
+                  controller: descriptionController,
+                  style: textStyle,
+                  decoration: InputDecoration(
+                    labelText: 'Description',
+                    labelStyle: textStyle,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
                     ),
                   ),
-                ],
+                  onChanged: (String value) {
+                    debugPrint('Something changed in Description Text Field');
+                  },
+                ),
               ),
-            ),
-          ],
+              Padding(
+                padding: EdgeInsets.only(
+                  top: _minimumPadding * 3,
+                  bottom: _minimumPadding * 3,
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: RaisedButton(
+                        color: Theme.of(context).primaryColorDark,
+                        textColor: Theme.of(context).primaryColorLight,
+                        child: Text(
+                          'Save',
+                          textScaleFactor: 1.5,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            debugPrint('Save state button clicked');
+                          });
+                        },
+                      ),
+                    ),
+                    Container(
+                      width: 5.0,
+                    ),
+                    Expanded(
+                      child: RaisedButton(
+                        color: Theme.of(context).primaryColorDark,
+                        textColor: Theme.of(context).primaryColorLight,
+                        child: Text(
+                          'Delete',
+                          textScaleFactor: 1.5,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            debugPrint('Delete state button clicked');
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
