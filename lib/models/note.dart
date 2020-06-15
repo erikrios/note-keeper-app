08a7519bbe78807742315f9d661247dc42541192
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-06-15 14:06:17 +0700

    Add getter

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,4 +1,22 @@
 class Note {
+  int _id;
+  String _title;
+  String _description;
+  String _date;
+  int _priority;
 
-  
+  Note(this._title, this._date, this._priority, [this._description]);
+
+  Note.withId(this._id, this._title, this._date, this._priority,
+      [this._description]);
+
+  int get id => _id;
+
+  String get title => _title;
+
+  String get description => _description;
+
+  String get date => _date;
+
+  int get priority => _priority;
 }
