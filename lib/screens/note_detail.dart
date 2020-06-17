c6f3227b624b56612b797fd461b6d2a45a465886
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-06-17 19:57:04 +0700

    Add a method to save data to database

--- lib/screens/note_detail.dart
+++ lib/screens/note_detail.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:note_keeper_app/models/note.dart';
+import 'package:note_keeper_app/utils/database_helper.dart';
 
 class NoteDetail extends StatefulWidget {
   final String _appBarTitle;
@@ -24,6 +26,9 @@ class _NoteDetailState extends State<NoteDetail> {
     'High',
     'Low',
   ];
+
+  DatabaseHelper helper = DatabaseHelper();
+
   String _priorityValue;
 
   TextEditingController titleController = TextEditingController();
@@ -210,5 +215,28 @@ class _NoteDetailState extends State<NoteDetail> {
   // Update the description of Note object
   void updateDescription() {
     note.description = descriptionController.text;
+  }
+
+  // Save data to database
+  void _save() async {
+    _navigateToList();
+    note.date = DateFormat.yMMMd().format(DateTime.now());
+    int result;
+    if (note.id != null) {
+      // Case 1: Update operation
+      result = await helper.updateNote(note);
+    } else {
+      // Case 2: Insert operation
+      result = await helper.insertNote(note);
+    }
+
+    if (result != 0) {
+      // Success
+      // _showAlertDialog('Status', 'Note Saved Successfully');
+    } else {
+      // Failure
+      // _showAlertDialog('Status', 'Note Saved Successfully');
+
+    }
   }
 }
